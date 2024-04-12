0b1aef590e107ccc4af9cda4a529b1963c4cfbd1
Francis Valoy <francisalcantara001@gmail.com>
2024-04-12 19:33:22 -0400

    parse request start line

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 Console.WriteLine("Starting server...");
 
@@ -10,8 +11,42 @@ server.Start();
 var socket = server.AcceptSocket();
 Console.WriteLine("Connection accepted.");
 
-string response = "HTTP/1.1 200 OK\r\n\r\n";
-byte[] sendBytes = System.Text.Encoding.ASCII.GetBytes(response);
-socket.Send(sendBytes);
+var responseBytes = new byte[255];
+int bytesReceived = socket.Receive(responseBytes);
 
-Console.WriteLine("Message sent /> : " + response);
+string request = Encoding.ASCII.GetString(responseBytes);
+RequestStartLine sl = RequestStartLine.Create(request);
+
+string okResponseStartLine = "HTTP/1.1 200 OK\r\n\r\n";
+string notFoundResponseStartLine = "HTTP/1.1 404 NotFound\r\n\r\n";
+
+if (sl.Path == "/")
+{
+    byte[] sendBytes = Encoding.ASCII.GetBytes(okResponseStartLine);
+    socket.Send(sendBytes);
+    Console.WriteLine(okResponseStartLine + "response sended");
+}
+else
+{
+    byte[] sendBytes = Encoding.ASCII.GetBytes(notFoundResponseStartLine);
+    socket.Send(sendBytes);
+    Console.WriteLine(notFoundResponseStartLine + "response sended");
+}
+
+record struct RequestStartLine(string HttpMethod, string Path, string HttpVersion)
+{
+    public static RequestStartLine Create(string request)
+    {
+        var startLine = request
+                .Split("\r\n")
+                .First()
+                .Split(' ');
+        return new(startLine[0], startLine[1], startLine[2]);
+    }
+
+    public override string ToString()
+        => $"{HttpMethod} {Path} {HttpVersion}";
+
+    public byte[] ToByte()
+        => Encoding.ASCII.GetBytes(ToString());
+}
