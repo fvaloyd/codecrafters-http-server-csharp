47746f52f1a4126d663ef39575caf16718a72185
Francis Valoy <francisalcantara001@gmail.com>
2024-04-12 18:36:01 -0400

    response with 200OK

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,17 @@
 using System.Net;
 using System.Net.Sockets;
 
-Console.WriteLine("Logs from your program will appear here!");
+Console.WriteLine("Starting server...");
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 
 server.Start();
 
-server.AcceptSocket();
+var socket = server.AcceptSocket();
+Console.WriteLine("Connection accepted.");
+
+string response = "HTTP/1.1 200 OK\r\n\r\n";
+byte[] sendBytes = System.Text.Encoding.ASCII.GetBytes(response);
+socket.Send(sendBytes);
+
+Console.WriteLine("Message sent /> : " + response);
